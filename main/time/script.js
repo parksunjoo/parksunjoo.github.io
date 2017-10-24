46cca29ecb95ae98323f5d0051860d637f7aa205
parksunjoo <qrktjswn@naver.com>
2017-10-24 18:38:00 +0900

    수정

---
+++ main/time/script.js
@@ -0,0 +1,14 @@
+var init = function(){
+  var sec = document.getElementById('sec');
+  var min = document.getElementById('min');
+  var hour = document.getElementById('hour');
+
+  function intervalHandler() {
+    time = new Date();
+    sec.innerHTML = time.getSeconds();
+    min.innerHTML = time.getMinutes();
+    hour.innerHTML = time.getHours();
+  }
+  setInterval(intervalHandler, 1000);
+};
+document.addEventListener('DOMContentLoaded', init);
